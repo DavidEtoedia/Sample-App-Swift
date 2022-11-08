79c2732d5ea1b26bc96907e45ce2a96208615dd2
DavidEtoedia <etoedia@gmail.com>
2022-11-08 12:35:51 -0800

    Initial Commit

---
+++ Sample-App-Swift/Sample_App_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample_App_SwiftApp.swift
+//  Sample-App-Swift
+//
+//  Created by Inyene Etoedia on 08/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Sample_App_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
